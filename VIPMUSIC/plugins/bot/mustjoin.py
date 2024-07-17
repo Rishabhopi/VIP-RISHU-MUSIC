f6d6b580d394c5dc942e112576811ccd89a0299a
Rishabh <152057079+Rishabhopi@users.noreply.github.com>
2024-07-17 11:13:21 +0530

    Update mustjoin.py

--- VIPMUSIC/plugins/bot/mustjoin.py
+++ VIPMUSIC/plugins/bot/mustjoin.py
@@ -6,7 +6,7 @@ from VIPMUSIC import app
 
 #--------------------------
 
-MUST_JOIN = "GODXNEXU"
+MUST_JOIN = "Ur_rishu_143"
 #------------------------
 @app.on_message(filters.incoming & filters.private, group=-1)
 async def must_join_channel(app: Client, msg: Message):
